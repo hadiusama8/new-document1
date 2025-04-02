d52ac95fdec3235891a65e9cbc4537fba5309796
hadiusama8 <hadi.usama8@gmail.com>
2025-04-03 00:28:14 +0500

    configs in .env

--- server/firebase.js
+++ server/firebase.js
@@ -1,14 +1,22 @@
-// Import Firebase Admin SDK
+require('dotenv').config();
 const admin = require('firebase-admin');
 
-// Load the service account key JSON file
-const serviceAccount = require('./new-document-d5be0-firebase-adminsdk-fbsvc-456cf9adb0.json');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
-// Initialize Firebase Admin
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://new-document-d5be0.firebaseio.com", // Use Realtime Database or Firestore
+  credential: admin.credential.cert(serviceAccount)
 });
 
-// Export admin instance for use in other files
 module.exports = admin;
